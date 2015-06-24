e4257823c7a591f3125e3756a84ea82487cbc9d4
Tech Camp 103 <tech103@fermium.cs.dixie.edu>
2015-06-24 10:41:50 -0600

    I LOVE ANGULAR, also i made a todo app with it

---
+++ Javascript/Angular basics/javascripts/timer.fct.js
@@ -0,0 +1,15 @@
+(function(){
+	angular.module('myApp')
+		.factory('Timer', ['$interval',
+		function($interval)
+		{
+			var Timer =
+			{
+				interval: function(fn)
+				{
+					$interval(fn, 1000);
+				}
+			};
+			return Timer;
+		}]);
+}).call(this);
